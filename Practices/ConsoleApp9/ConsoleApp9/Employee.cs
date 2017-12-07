73f8e564600d74dd8805c177f1d96a74a150ff67
Mikhail <mybestdog@gmail.com>
2017-12-07 09:50:13 +0600

    PRACTICE #9 DONE

---
+++ Practices/ConsoleApp9/ConsoleApp9/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public struct Employee
+    {
+        Vacancies Vacancy { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public Employee(Vacancies vacancy,string name,int salary,DateTime date)
+        {
+            Vacancy = vacancy;
+            Name = name;
+            Salary = salary;
+            EmploymentDate = date;
+        }
+    }
+}
