acc38b3a69084dc7bcdd2b70470977c5c356faa6
Mikhail <mybestdog@gmail.com>
2017-12-28 11:33:50 +0600

    HOMEWORK #12 DONE

---
+++ HomeWorks/ConsoleApp12/ConsoleApp12/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Player
+    {
+        public static int playerCount = 0;
+        public string Name { get; set; }
+        public List<Karta> Cards { get; set; }
+        public Player()
+        {
+            playerCount++;
+            Name = "Player #" + playerCount;
+            Cards = new List<Karta>();
+        }
+    }
+}
