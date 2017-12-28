a838f397c07b2de97461431b576272f50089e2d8
Mikhail <mybestdog@gmail.com>
2017-12-28 10:00:07 +0600

    HOMEWORK #10 DONE

---
+++ HomeWorks/ConsoleApp10/ConsoleApp10/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Team
+    {
+        TeamLeader TeamLeader { get; set; }
+        List<Worker> Workers { get; set; }
+        public Team()
+        {
+            TeamLeader = new TeamLeader("Mikhail");
+            Workers = new List<Worker>() {
+                new Worker("Worker #1"),
+                new Worker("Worker #2"),
+                new Worker("Worker #3"),
+            };
+        }
+    }
+}
---
+++ HomeWorks/ConsoleApp10/ConsoleApp10/TeamLeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class TeamLeader : IWorker
+    {
+        public string Name { get; set; }
+        public TeamLeader(string value)
+        {
+            Name = value;
+        }
+    }
+}
