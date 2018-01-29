b49ff5c9d150fc9392ab5130525c18fba110982f
Mikhail <mybestdog@gmail.com>
2018-01-29 10:44:36 +0600

    HOMEWORK #6,#8,#9 DONE

---
+++ WPF/WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            sum.Text = (int.Parse(fuelBox92.Text) + int.Parse(fuelBox95.Text)+ int.Parse(fuelBox98.Text)+ int.Parse(food1.Text)+int.Parse(food2.Text)+ int.Parse(food3.Text)).ToString();
+        }
+    }
+}
---
+++ WPF/WpfApp9/WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Images (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            openDialog.ShowDialog();
+            if (openDialog.FileName.Length > 0)
+            {
+                ImageBrush brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri(openDialog.FileName));
+                canvas.Background = brush;
+                imageName.Content = openDialog.SafeFileName;
+            }
+        }
+    }
+}
